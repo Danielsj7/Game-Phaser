458711ecf56b3ac58d2fd304bc85434e01655218
Danielsj7 <dsotojaimes@gmail.com>
2019-11-11 21:59:06 -0500

    Pong integración

---
+++ public/src/bootloader.js
@@ -0,0 +1,20 @@
+class Bootloader extends Phaser.Scene {
+  constructor() {
+    super({ key: "Bootloader" });
+  }
+  preload() {
+    this.load.on("complete", () => { //metodo escucha
+      this.scene.start("Scene_play"); //llamar la Ecena
+    });
+    this.load.image("ball", "./assets/ball.png");
+    this.load.image("izquierda", "./assets/left_pallete.png");
+    this.load.image("derecha", "./assets/right_pallete.png");
+    this.load.image("linea", "./assets/separator.png");
+
+
+  }
+
+
+}
+
+export default Bootloader;
